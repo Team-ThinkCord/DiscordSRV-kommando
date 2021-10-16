a4eb38343ee7278943cfd0b7898f00c05584c357
NAME <mail@gmail.com>
2021-10-16 12:05:17 +0900

    Initial commit

---
+++ Icetang0123/main.js
@@ -0,0 +1,25 @@
+const Plugins = require('discord-kommando.js-plugins');
+const Discord = require('discord.js');
+const plugin = new Plugin.plugin(Plugins.perms.ALL, {
+    channelID: null
+});
+
+const srvSend = function(channel, message) {
+    channel.send(`[DiscordSRV-kommando]: ${message}`);
+}
+
+plugin.on('messageCreate', (msg) => {
+    if (!plugin.config.channelID) throw new TypeError("Cannot read property 'channelID' of null");
+    if (!msg.channel.id == (channelID + "")) return;
+    var output = "";
+    try {
+        output = eval(msg);
+    } catch(err) {
+        return srvSend(msg.channel, err.toString());
+    }
+    srvSend(msg.channel, output);
+});
+
+plugin.on('error', (err, client) => srvSend(client.channels.cache.get(plugin.config.chanelID), err.stack));
+
+module.exports = plugin;
---
+++ node_modules/discord-kommando.js-plugins/README.md
@@ -0,0 +1,5 @@
+# discord-kommando.js-plugins
+The official discord-kommando.js plugin creation module
+
+## Docs
+[Docs here](https://discord-kommando-plugins.js.org)
